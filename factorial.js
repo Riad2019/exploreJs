1664080e0ebd611d49b8cd9bf8bfec351bc71dc7
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-07-23 19:56:45 -0400

    factorial number

--- factorial.js
+++ factorial.js
@@ -1,9 +1,15 @@
 /**3!=1*2*3
 4!=1*2*3*4
 5!=1*2*3*4 */
-
-var factorial=1;
-for(var i=1 ; i<=10 ; i++){
-    factorial= factorial*i;
-    console.log(i , factorial);
+function fac ( n){
+    var factorial=1;
+    for(var i=1 ; i<=n ; i++){
+        factorial= factorial*i;
+        
+        
+    }
+    return factorial;
+    
 }
+const result = fac(5);
+console.log(result);
